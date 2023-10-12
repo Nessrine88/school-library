90117e03c1cb7d11d580a4aeb2d09590ea093e1a
unknown <macherki.nessrine@yahoo.com>
2023-10-12 09:01:24 +0100

    add decorator and nameable classes

--- person.rb
+++ person.rb
@@ -1,12 +1,26 @@
-class Person
+require_relative 'nameable'
+require_relative 'decorator'
+
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'Subclasses must implement this method'
+  end
+end
+
+class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :parent_permission
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -19,3 +33,12 @@ class Person
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts "Original Name: #{person.correct_name}"
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts "Capitalized Name: #{capitalized_person.correct_name}"
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
